2bcc4b5a84a532e3a9c48230668774008fee3396
Rafal <rafal@pkey.pl>
2013-10-02 22:30:38 +0200

    added

--- depot/Modules/depot32/test/unit/user_test.rb
+++ depot/Modules/depot32/test/unit/user_test.rb
@@ -4,4 +4,8 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "There is one user" do
+    assert  1 < User.count, "The was no users defined"
+  end
 end
